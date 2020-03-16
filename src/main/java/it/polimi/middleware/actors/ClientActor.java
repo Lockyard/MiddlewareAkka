32c43d61dfaf4537ae300ab6fb08cdc13c45567b
Lockyard <hyperl@hotmail.it>
2020-03-16 19:01:25 +0100

    gitignore for gradle

---
+++ src/main/java/it/polimi/middleware/actors/ClientActor.java
@@ -0,0 +1,10 @@
+package it.polimi.middleware.actors;
+
+import akka.actor.AbstractActor;
+
+public class ClientActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
+}
