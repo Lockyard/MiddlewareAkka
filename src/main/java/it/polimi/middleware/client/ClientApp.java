c769d10f86ad9daad3dda8b6fb8d0f7183ef9a64
Lockyard <hyperl@hotmail.it>
2020-04-02 18:20:34 +0200

    basic build now working with maven shade plugin included. Requires build done by "mvn package"

--- src/main/java/it/polimi/middleware/client/ClientApp.java
+++ src/main/java/it/polimi/middleware/client/ClientApp.java
@@ -4,22 +4,37 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigValueFactory;
 import it.polimi.middleware.messages.GetMsg;
 import it.polimi.middleware.messages.ReplyGetMsg;
 import it.polimi.middleware.messages.ReplyPutMsg;
 
 import java.io.File;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class ClientApp {
 
     public static void main (String[] args) {
-        final Config conf = ConfigFactory.parseFile(new File("conf/client.conf"));
+
+        if(args.length > 0 && args [0].equalsIgnoreCase("--help"))
+            System.out.println("args: [serverAddress serverPort].\nIf not provided, default values will be used" +
+                    "Use 1 argument to insert the address, or use 'd' as 1st arg to set default address but specify port");
+
+        //set server address and port from args. If not provided, default ones are used
+        final String serverAddress = (args.length > 0 && !args[0].equals("d")) ? args[0] : "127.0.0.1";
+        final String serverPort = args.length > 1 ? args [1] : "54333";
+
+        //Load config resource file and start the ClientActor
+        final Config conf = ConfigFactory.load("conf/client.conf");
+
         final ActorSystem sys = ActorSystem.create("Client", conf);
-        final ActorRef client = sys.actorOf(ClientActor.props("ServerSystem@127.0.0.1:9000"), "ClientActor");
+        final ActorRef client = sys.actorOf(
+                ClientActor.props("ServerClusterSystem@"+serverAddress+":"+serverPort), "ClientActor");
 
         final Scanner scanner = new Scanner(System.in);
 
+        //Loop which handles messages to send to the ClientActor, from which will be sent to the server
         while (true) {
             final String content = scanner.nextLine();
             if (content.equals("quit")) {
--- src/main/java/it/polimi/middleware/server/ServerApp.java
+++ src/main/java/it/polimi/middleware/server/ServerApp.java
@@ -3,26 +3,48 @@ package it.polimi.middleware.server;
 import akka.actor.ActorSystem;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigValueFactory;
 import it.polimi.middleware.server.actors.MasterNode;
 
-import java.io.File;
+import java.util.Collections;
 import java.util.Scanner;
 
+/**
+ * Main class for server. Starts up the master node and its actorSystem.
+ */
 public class ServerApp {
 
     public static void main (String[] args) {
+        try {
 
-        final Config conf = ConfigFactory.parseFile(new File("conf/cluster.conf"));
-        final ActorSystem sys = ActorSystem.create("ServerSystem", conf);
-        sys.actorOf(MasterNode.props(), "MasterNode");
+            final String port = args.length == 0 ? "54333" : args[0];
+            //load config file of cluster and start MasterNode
+            final Config conf = ConfigFactory.load("conf/cluster.conf")
+                    .withValue("akka.remote.netty.tcp.port", ConfigValueFactory.fromAnyRef(port)) //
+                    .withValue("akka.cluster.roles", ConfigValueFactory.fromIterable(Collections.singletonList("master")));
 
-        System.out.println("Server started, enter 'quit' to terminate");
-        Scanner s = new Scanner(System.in);
-        String input = "";
-        while(!input.equalsIgnoreCase("quit")) {
-            input = s.nextLine();
+            final ActorSystem sys = ActorSystem.create("ServerClusterSystem", conf);
+            sys.actorOf(MasterNode.props(), "MasterNode");
+
+            //simple loop to handle basic commands.
+            System.out.println("Server started, enter 'quit' to terminate");
+            Scanner s = new Scanner(System.in);
+            String input = "";
+            while(!input.equalsIgnoreCase("quit")) {
+                input = s.nextLine();
+            }
+
+            //quit command entered, quit server
+            sys.terminate();
+            s.close();
+
+
+        } catch (Exception e) {
+            System.out.println("An error in ServerApp occurred:\n");
+            e.printStackTrace();
+
+            System.exit(-1);
         }
-        sys.terminate();
-        s.close();
+
     }
 }
